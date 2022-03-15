a835a768025053422a50dfc914e98760f0d7c9f5
Vanessa Yuen <imtvwy@student.ubc.ca>
2022-03-15 13:27:06 -0700

    Merge Card 2

--- app.R
+++ app.R
@@ -1,19 +1,68 @@
 library(dash)
-library(dashBootstrapComponents)
+#library(dashBootstrapComponents)
+library(dashHtmlComponents)
 library(ggplot2)
 library(plotly)
 library(tidyr)
 library(dplyr)
+library(tidyverse)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
 df <- readr::read_csv(here::here("data", "raw", "world-data-gapminder_raw.csv"))
+
 
 df[is.na(df)] <- 0
 
 df_year_region <- df %>%
     group_by(year, region) %>%
     summarise(sum_co2_per_capita = sum(co2_per_capita))
+
+
+marks_card2 <- list("1918" = "1918", 
+                "1928" = "1928",
+                "1938"="1938",
+                "1948" = "1948",
+                "1958" = "1958",
+                "1968" = "1968",
+                "1978" = "1978",
+                "1988" = "1988",
+                "1998" = "1998",
+                "2008" = "2008",
+                "2018" = "2018"
+                )
+
+plot <- function(year_range, filter) {
+  if(filter=='') {
+    df_by_year <- df %>%
+      filter(year>=year_range[1] & year<year_range[2]) %>%
+      group_by(year) %>%
+      summarize(children_per_woman = mean(children_per_woman))
+    p <- ggplot(df_by_year, 
+          aes(y = children_per_woman,
+              x = year)) +
+          geom_line() +
+          labs(y="Children per woman", x="Year") +
+          ggtitle("Average Number of Children") +
+          theme(plot.title = element_text(hjust = 0.5))
+  } else {
+    df_by_year <- df %>%
+      filter(year>=year_range[1] & year<year_range[2]) %>%
+      group_by(!!sym(filter), year) %>%
+      summarize(children_per_woman = mean(children_per_woman))
+
+    p = ggplot(df_by_year, 
+            aes(y = children_per_woman,
+                x = year)) +
+            geom_line(aes(color=!!sym(filter))) +
+            labs(y="Children per woman", x="Year") +
+            ggtitle("Average Number of Children") +
+            theme(plot.title = element_text(hjust = 0.5))
+  }
+  return(p)
+}
+
+
 app$layout(
     div(
         list(
@@ -74,7 +123,21 @@ app$layout(
                     md = 6
                     ),
                     dbcCol(dbcCard(className = "m-3 p-3", div(
-                        "Card 2"
+                        list(
+                            dccGraph(id='plot_area'),
+                            htmlLabel('Zoom in years: '),
+                            dccRangeSlider(min=1918, max=2018, step=1-10, value=list(1918, 2018),  id='year_range_slider',
+                                        marks=marks_card2
+                            ),
+                            htmlLabel('See breakdown number by: '),
+                            dccDropdown(
+                            options=list(
+                            list(label='All', value=''),
+                            list(label='Income Group', value='income_group'),
+                            list(label='Region', value='region')),
+                            value='', clearable=FALSE, id='filter_dropdown')
+                            )
+
                     )), md = 6)
                 )
             ),
@@ -109,6 +172,18 @@ app$callback(
         ggplotly(p) %>% layout()
     }
 )
+
+app$callback(
+    output('plot_area', 'figure'),
+    list(
+      input('year_range_slider', 'value'),
+      input('filter_dropdown', 'value')
+      ),
+    function(year_range, filter) {
+      return(ggplotly(plot(year_range, filter)))
+    }
+)
+
 
 
 app$run_server(host = "0.0.0.0")
